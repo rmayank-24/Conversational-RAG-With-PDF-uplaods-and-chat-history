50605881c7dc7d2c87761e74b457dd038e62e4c7
rmayank-24 <137880416+rmayank-24@users.noreply.github.com>
2025-07-05 12:46:10 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 ## RAG Q&A Conversation With PDF Including Chat History
+
+try:
+    import pysqlite3
+    import sys
+    sys.modules["sqlite3"] = pysqlite3
+except ImportError:
+    pass 
 import streamlit as st
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
